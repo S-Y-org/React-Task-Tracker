4d32060e194615c966447cdc13b547401d2b70c2
IT21440236 <it21440236@my.sliit.lk>
2023-06-26 17:31:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
+    //If you at the code inside the return statement, you'll see that it's just HTML. No it's not. It's JSX.
+    // JSX stands for "JavaScript XML." It is a syntax extension for JavaScript that allows you to write HTML-like code within JavaScript. JSX is commonly used in React.js to define the structure and appearance of user interfaces. It provides a convenient way to create and manipulate DOM elements, making it easier to build dynamic and interactive web applications.
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
